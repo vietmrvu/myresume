ea93b9a9906e05edb99aacb5ee18f08e6bbdfaaa
vietmrvu <91784212+vietmrvu@users.noreply.github.com>
2021-10-06 20:07:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var a = 1
+var button = document.querySelectorAll("button")
+function new(i){
+    var h4 = document.quertSelectorAll("#one")
+    h4.textContent = "Oh, you like it."
+    var h3 = document.quertSelectorAll("#two")
+    h3.textContent = "You don't like it, do you."
+    i.onclick = 0 
+    
+}
+button.onclick = new(button);
