b40dddbe8603b56f54db8f10e7411f6772eb739c
vietmrvu <91784212+vietmrvu@users.noreply.github.com>
2021-10-08 06:32:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-var button = document.getElementById("first")
-var w_button = document.getElementById("second")
 function more(){
     var text_button = document.getElementById("one")
     text_button.textContent = "Oh, you like it."
@@ -9,8 +7,6 @@ function moreoh(){
     var text_w_button = document.getElementById("two")
     text_w_button.textContent = "You don't like it, do you."
 }
-button.onclick = more;
-w_button.onclick = moreoh;
 function init(){
     var button = document.getElementById("first")
     button.onclick = more;
