6da3d04b69e6babecb37c182698ccac250364d9c
vietmrvu <91784212+vietmrvu@users.noreply.github.com>
2021-10-07 09:08:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var text_button = document.querySelector("#one")
-var text_w_button = document.querySelector("#two")
-var button = document.querySelector("#first")
-var w_button = document.querySelector("#second")
+var text_button = document.getElementById("one")
+var text_w_button = document.getElementById("two")
+var button = document.getElementById("first")
+var w_button = document.getElementById("second")
 function more(){
-    var text_button = document.querySelector("#one")
+    var text_button = document.getElementById("one")
     text_button.textContent = "Oh, you like it."
 }
 function moreoh(){
-    var text_w_button = document.querySelector("#two")
+    var text_w_button = document.getElementById("two")
     text_w_button.textContent = "You don't like it, do you."
 }
 button.onclick = more;
